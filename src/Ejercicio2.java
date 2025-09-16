a5084f4d534e7941bed5c861b2a91c49b43a7e9e
jonpitsaf <jonaspitarch06@gmail.com>
2025-09-16 08:27:48 +0200

    Ejercicio 2 completo

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -8,40 +8,49 @@ public class Ejercicio2 {
 
         Scanner teclat = new Scanner(System.in);
         String ruta = ".";
+        boolean masinfo = false;
         while (!ruta.equals("")) {
             System.out.println("Dime la ruta a utilizar");
             ruta = teclat.nextLine();
+            System.out.println("Dime si quieres mas informacion");
+            String m = teclat.nextLine();
+            if (m.equalsIgnoreCase("Si")){
+                masinfo = true;
+            }else if (m.equalsIgnoreCase("No")){
+                masinfo = false;
+            }
             try {
                 File a = new File(ruta);
-                mostrarRuta(a);
+                mostrarRuta(a, masinfo);
             }
             catch (FileNotFoundException e) {
                 System.out.println("Gracias por usar el terminal");
             }
-            System.out.println("Quieres mas informacion? ");
-            String informacion = teclat.nextLine();
-            if (informacion.equalsIgnoreCase("si")) {
-
-            }
 
         }
     }
 
-    static void mostrarRuta(File x) throws FileNotFoundException {
+    static void mostrarRuta(File x , boolean masinfo) throws FileNotFoundException {
         if(!x.exists()){
             throw new FileNotFoundException("ERROR - Ruta invalida");
         }
-        mostrartipo(x);
-    }
-
-    static void mostrartipo(File x ){
         File [] lista = x.listFiles();
-        for (File l : lista){
-            if (l.isDirectory()){
-                System.out.println("[D] " + l.getName());
+        if (masinfo != true){
+            for (File l : lista){
+                if (l.isDirectory()){
+                    System.out.println("[D] " + l.getName());
+                }
+                if (l.isFile()){
+                    System.out.println("[A] " + l.getName());
+                }
             }
-            if (l.isFile()){
-                System.out.println("[A] " + l.getName());
+        }
+        if (masinfo == true){
+            for (File l : lista){
+                if (l.isDirectory()){
+                    System.out.println("[D] " + l.getName() + " la ultima modificacion es el " + l.lastModified() + " el " +
+                            "tamaño es " + l.length() + " bytes" );
+                }
             }
         }
     }
